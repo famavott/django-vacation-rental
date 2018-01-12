974e146470a5e63c4cfc027685b5559a2f8cea56
Matt Favoino <mattfavoino@gmail.com>
2018-01-12 14:07:09 -0800

    Implemented modal/lightbox on one photo. Moved js scripts around to make stuff work.

---
+++ vacation_rental/vacation_rental/static/vacation_rental/app.js
@@ -0,0 +1,6 @@
+'use strict'
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
